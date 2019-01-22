bb09cd36c17e2999ac17125d17826cb1f21bfd4c
Corina Pelichowski <corina@danitt.com>
2019-01-22 18:38:04 +1000

    addition of cat image when clicking on logo

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-let myHeading = document.querySelector('h1');
-  myHeading.textContent = "Hello World";
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+  var mySrc = myImage.getAttribute('src');
+  if(mySrc === "images/firefox.png") {
+    myImage.setAttribute ('src', 'images/cat.png');
+  }else {
+    myImage.setAttribute ('src', "images/firefox.png");
+  }
+}
