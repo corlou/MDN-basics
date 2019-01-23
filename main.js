a256ecb33a740ef36381cd9901fef0151e338e2a
Corina Pelichowski <corina@danitt.com>
2019-01-23 11:53:50 +1000

    adding a clickable button

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//Click on Mozilla Logo to change image
 let myImage = document.querySelector('img');
 
 myImage.onclick = function(){
@@ -7,4 +9,22 @@ myImage.onclick = function(){
   }else {
     myImage.setAttribute ('src', "images/firefox.png");
   }
+}
+
+//"Change User" button
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+//Set user name
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool ' + myName;
+}
+
+if(!LocalStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool' + storedName;
 }
